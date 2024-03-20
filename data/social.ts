6bb3108406c9063b70b2e7de3969079f28f198c0
liucj <2113191149@qq.com>
2024-03-21 02:35:15 +0800

    修改社交连接     github

--- data/social.ts
+++ data/social.ts
@@ -1,13 +1,13 @@
 export type Social = {
   github?: string
-  twitter?: string
+  // twitter?: string
   juejin?: string
-  qq?: string
-  wx?: string
+  // qq?: string
+  // wx?: string
   cloudmusic?: string
-  zhihu?: string
+  // zhihu?: string
   email?: string
-  discord?: string
+  // discord?: string
 }
 
 type SocialValue = {
@@ -18,15 +18,15 @@ type SocialValue = {
 }
 
 const social: Social = {
-  github: 'https://github.com/kuizuo',
-  twitter: 'https://twitter.com/kuizuo',
+  github: 'https://github.com/oceanlcj',
+  // twitter: 'https://twitter.com/kuizuo',
   juejin: 'https://juejin.cn/user/1565318510545901',
-  qq: 'https://img.kuizuo.cn/qq.png',
-  wx: 'https://img.kuizuo.cn/wechat.png',
+  // qq: 'https://img.kuizuo.cn/qq.png',
+  // wx: 'https://img.kuizuo.cn/wechat.png',
   // zhihu: 'https://www.zhihu.com/people/kuizuo',
   // cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
   email: 'mailto:hi@kuizuo.cn',
-  discord: 'https://discord.gg/M8cVcjDxkz',
+  // discord: 'https://discord.gg/M8cVcjDxkz',
 }
 
 const socialSet: Record<keyof Social | 'rss', SocialValue> = {
@@ -42,36 +42,36 @@ const socialSet: Record<keyof Social | 'rss', SocialValue> = {
     icon: 'simple-icons:juejin',
     color: '#1E81FF',
   },
-  twitter: {
-    href: social.twitter,
-    title: 'Twitter',
-    icon: 'ri:twitter-line',
-    color: '#1da1f2',
-  },
-  discord: {
-    href: social.discord,
-    title: 'Discord',
-    icon: 'ri:discord-line',
-    color: '#5A65F6',
-  },
-  qq: {
-    href: social.qq,
-    title: 'QQ',
-    icon: 'ri:qq-line',
-    color: '#1296db',
-  },
-  wx: {
-    href: social.wx,
-    title: '微信',
-    icon: 'ri:wechat-2-line',
-    color: '#07c160',
-  },
-  zhihu: {
-    href: social.zhihu,
-    title: '知乎',
-    icon: 'ri:zhihu-line',
-    color: '#1772F6',
-  },
+  // twitter: {
+  //   href: social.twitter,
+  //   title: 'Twitter',
+  //   icon: 'ri:twitter-line',
+  //   color: '#1da1f2',
+  // },
+  // discord: {
+  //   href: social.discord,
+  //   title: 'Discord',
+  //   icon: 'ri:discord-line',
+  //   color: '#5A65F6',
+  // },
+  // qq: {
+  //   href: social.qq,
+  //   title: 'QQ',
+  //   icon: 'ri:qq-line',
+  //   color: '#1296db',
+  // },
+  // wx: {
+  //   href: social.wx,
+  //   title: '微信',
+  //   icon: 'ri:wechat-2-line',
+  //   color: '#07c160',
+  // },
+  // zhihu: {
+  //   href: social.zhihu,
+  //   title: '知乎',
+  //   icon: 'ri:zhihu-line',
+  //   color: '#1772F6',
+  // },
   email: {
     href: social.email,
     title: '邮箱',
