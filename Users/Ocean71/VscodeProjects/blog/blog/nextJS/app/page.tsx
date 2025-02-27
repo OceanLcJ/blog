df7eb09f64f71525c4c180c887787434d3b32cb3
liucj <2113191149@qq.com>
2025-02-27 23:13:41 +0800

    /app/page.tsx 文件:
    将注释格式化为一致的风格。
    确保导入语句和组件定义的格式正确。
    /app/ui/home.module.css 文件:
    
    确保 CSS 规则的格式正确，没有多余的空格或换行。
    这些修改主要是为了提高代码的可读性和一致性。

---
+++ Users/Ocean71/VscodeProjects/blog/blog/nextJS/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/app/ui/home.module.css';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className={styles.shape} />
+    </main>
+  );
+}
