57d1114f2debd534142742ab42818610b5423ec2
liucj <2113191149@qq.com>
2025-02-28 00:03:34 +0800

    删除无关内容

--- Users/Ocean71/VscodeProjects/blog/blog/nextJS/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/app/ui/home.module.css';
-
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
-    </main>
-  );
-}
